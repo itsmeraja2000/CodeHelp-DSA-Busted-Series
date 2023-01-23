3cd57f74d7b86adb1cc8e0e4f86967336477a8f1
Raja <121866019+itsmeraja2000@users.noreply.github.com>
2023-01-23 18:37:42 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -22,50 +22,54 @@
 
 ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
-    
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
+Node<int>* solve(Node<int>* first, Node<int>* second){
+    //if only one element is present in first list
+    if(first->next ==NULL){
+        first ->next = second;
+        return first;
+    }
+    Node<int>* curr1 = first;
+    Node<int>* next1 = curr1 -> next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = curr2 -> next;
+
+    while(next1 != NULL && curr2 != NULL ){
+        if((curr2->data >= curr1-> data) && (curr2->data <= next1 -> data)){
             
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
+            curr1->next = curr2;
+            next2 = curr2-> next;
+            curr2->next = next1;
+
             curr1 = curr2;
             curr2 = next2;
+        } 
+        else{
+            //curr1 and next 1 ko age bado
+            curr1 = next1;
+            next1 = next1->next;
+
+            if(next1 == NULL){
+                curr1->next = curr2;
+                return first;
+            }
         }
-        else {
-            
-        }
-        
-        
     }
-    
-    
+    return first;
+
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
 {
+    // Write your code here.
     if(first == NULL)
         return second;
-    
     if(second == NULL)
         return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
+
+    if(first->data <= second -> data)
+        return solve(first,second);
     else
-    {
-        solve(second, first);
-    }
-    
-    
+        return solve(second,first);
+        
 }
+
